c3b33926dc01da4c0e6078a5ca729c8b6c035a59
Игорь <@IGOR-PC>
2015-05-16 23:11:54 +0300

    Работа с супер админом

---
+++ src/java/model/Person.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+    
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    @Column(name="id")
+    int id;
+    
+    @Column(name="fio")
+    String fio;
+    
+    @Column(name="birthDate")
+    String birthDate;
+    
+    @Column(name="dolzhnost")
+    String dolzhnost;
+
+    @Column(name="speciality")
+    String speciality;
+    
+    @Column(name="persontype")
+    String persontype = "worker";
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDolzhnost() {
+        return dolzhnost;
+    }
+
+    public void setDolzhnost(String dolzhnost) {
+        this.dolzhnost = dolzhnost;
+    }
+    
+}
--- src/java/model/Users.java
+++ src/java/model/Users.java
@@ -1,16 +1,10 @@
 package model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 
